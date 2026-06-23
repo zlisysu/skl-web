45e6fcfa78955dfcd94db65d763e8d6fa121b425
Zhe Li <lizhe5@mail.sysu.edu.cn>
2026-06-23 10:02:38 +0800

    Disable Django admin route

--- labsite/urls.py
+++ labsite/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.urls import include, path
-from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -9,7 +8,6 @@ from wagtail.documents import urls as wagtaildocs_urls
 from labsite.search import views as search_views
 
 urlpatterns = [
-    path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
